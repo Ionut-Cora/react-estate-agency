1b1f36718f9ead3a2b04c77fa8a5d91ee490d498
Ionut Cora <cora.ionut92@yahoo.com>
2024-01-31 15:24:05 +0000

    header carousel component

---
+++ src/components/HeaderCarousel.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Carousel from 'react-material-ui-carousel';
+import items from '../assets/items';
+import Item from '../components/Item';
+
+function HeaderCarousel() {
+  return (
+    <Carousel
+        next={ (next, active) => (`${active}, ${next}`) }
+        prev={ (prev, active) => (`${active}, ${prev}`) }
+        interval={5000}
+    >
+            {
+                items.map( (item, i) => <Item key={i} item={item} /> )
+            }
+        </Carousel>
+  )
+}
+
+export default HeaderCarousel;
