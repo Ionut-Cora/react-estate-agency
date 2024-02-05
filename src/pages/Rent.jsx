f2896d66a53d423bd4548096e49f45422c18a715
Ionut Cora <cora.ionut92@yahoo.com>
2024-02-05 11:12:23 +0000

    rent page

---
+++ src/pages/Rent.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import RentItem from '../components/RentItem';
+import Navigation from '../components/Navigation';
+import { Container } from '@mui/material';
+import SecondHeader from '../components/SecondHeader';
+import RentHeaderImg from '../assets/images/rent-header-img.jpg';
+import rentItems from '../assets/rentItems';
+import Footer from '../components/Footer';
+
+function Rent() {
+  return (
+    <div>
+        <Navigation navPosition='absolute' textColor='inherit' />
+        <SecondHeader title='Rent a property' img={RentHeaderImg} />
+        <Container>
+            {
+                rentItems.map(item => <RentItem key={item.id} id={item.id} title={item.title} address={item.address} price={item.price} shortDescription={item.shortDescription} bedrooms={item.bedrooms} bathrooms={item.bathrooms} livingrooms={item.livingrooms} img={item.image1} />)
+            }
+        </Container>
+        <Footer />
+    </div>
+  )
+}
+
+export default Rent;
