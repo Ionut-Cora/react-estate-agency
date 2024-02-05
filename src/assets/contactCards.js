54ba0d7f8ba744a65420cb34d280ee12a2f67c71
Ionut Cora <cora.ionut92@yahoo.com>
2024-02-05 11:31:19 +0000

    contact cards icons fix

--- src/assets/contactCards.js
+++ src/assets/contactCards.js
@@ -1,21 +1,21 @@
 const contactCards = [
     {
         id: 1,
-        icon: './icons/envelope-fill.svg',
+        icon: './assets/icons/envelope-fill.svg',
         title: 'Email',
         description: 'estate@agency.co.uk',
         url: 'mailto: react@example.com'
     },
     {
         id: 2,
-        icon: './icons/telephone-fill.svg',
+        icon: './assets/icons/telephone-fill.svg',
         title: 'Phone',
         description: '0900800700',
         url: 'tel:0900800700'
     },
     {
         id: 3,
-        icon: './icons/building-fill.svg',
+        icon: './assets/icons/building-fill.svg',
         title: 'Address',
         description: '1 Street Name, London',
         url: null
