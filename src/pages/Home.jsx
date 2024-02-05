302fb08fb82c01dea3114c12ba2eaf9d0f75347d
Ionut Cora <cora.ionut92@yahoo.com>
2024-02-05 11:06:38 +0000

    home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Box, Container } from '@mui/material';
+import Navigation from '../components/Navigation';
+import HeaderCarousel from '../components/HeaderCarousel';
+import AboutUsSection from '../components/AboutUsSection';
+import reviewImg from '../assets/images/review-img.jpg';
+import CardsSection from '../components/CardsSection';
+import KeyContactSection from '../components/KeyContactSection';
+import WhyChooseUs from '../components/WhyChooseUsComponents/WhyChooseUs';
+import ReviewsSection from '../components/ReviewsComponents/ReviewsSection';
+import ContactCardsSection from '../components/ContactPageComponents/ContactCardsSection';
+import Footer from '../components/Footer';
+import { grey } from '@mui/material/colors';
+
+function Home() {
+  return (
+    <div>
+        <Navigation navPosition='absolute' textColor='inherit' />
+        <HeaderCarousel />
+        <CardsSection />
+        <Box sx={{
+            backgroundColor: grey[100],
+            paddingY: '3rem'
+        }}>
+            <AboutUsSection />
+            <KeyContactSection />
+        </Box>
+        <WhyChooseUs />
+        <Box sx={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(${reviewImg})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }}>
+          <Container>
+            <ReviewsSection />
+          </Container>
+        </Box>
+        <Box sx={{
+          backgroundColor: grey[100],
+          paddingY: '3rem'
+        }}>
+          <ContactCardsSection />
+        </Box>
+        <Footer />
+    </div>
+  )
+}
+
+export default Home;
