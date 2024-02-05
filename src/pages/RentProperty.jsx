1b61c29ac163142511400088757d2a29502879f0
Ionut Cora <cora.ionut92@yahoo.com>
2024-02-05 11:13:56 +0000

    rent property page

---
+++ src/pages/RentProperty.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import rentItems from '../assets/rentItems';
+import Navigation from '../components/Navigation';
+import { useParams } from 'react-router-dom';
+import RentSaleProperty from '../components/RentSaleComponents/RentSaleProperty';
+import Footer from '../components/Footer';
+
+function RentProperty() {
+
+  const {id} = useParams();
+  const [rentProperty, setRentProperty] = useState(null);
+
+  useEffect(() => {
+    
+    let rentProperty = rentItems.find(rentProperty => rentProperty.id === parseInt(id));
+
+    if (rentProperty) {
+      setRentProperty(rentProperty);
+    }
+  }, [id])
+
+  return (
+    <div>
+        <Navigation navPosition='relative' textColor='black' />
+
+        {
+          rentProperty ? (
+            <RentSaleProperty propertyAddress={rentProperty.address} propertyImage={rentProperty.image1} propertyImages={rentProperty.images} propertyPrice={rentProperty.price} propertyLongDescription={rentProperty.longDescription} propertyBedrooms={rentProperty.bedrooms} propertyBathrooms={rentProperty.bathrooms} propertyLivingrooms={rentProperty.livingrooms} />
+          ) : (
+            null
+          )
+        }
+
+        <Footer />
+    </div>
+  )
+}
+
+export default RentProperty;
