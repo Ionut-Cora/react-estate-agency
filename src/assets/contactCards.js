12e1a374061496b0149b11e1d358cdd991b13027
Ionut Cora <cora.ionut92@yahoo.com>
2024-02-05 11:30:02 +0000

    why choose us cards images and logos fix

--- src/assets/contactCards.js
+++ src/assets/contactCards.js
@@ -1,21 +1,21 @@
 const contactCards = [
     {
         id: 1,
-        icon: './images/icons/envelope-fill.svg',
+        icon: './icons/envelope-fill.svg',
         title: 'Email',
         description: 'estate@agency.co.uk',
         url: 'mailto: react@example.com'
     },
     {
         id: 2,
-        icon: './images/icons/telephone-fill.svg',
+        icon: './icons/telephone-fill.svg',
         title: 'Phone',
         description: '0900800700',
         url: 'tel:0900800700'
     },
     {
         id: 3,
-        icon: './images/icons/building-fill.svg',
+        icon: './icons/building-fill.svg',
         title: 'Address',
         description: '1 Street Name, London',
         url: null
--- src/assets/whyChooseUsItems.js
+++ src/assets/whyChooseUsItems.js
@@ -3,22 +3,22 @@ const whyChooseUsItems = [
         id: 1,
         title: 'Renting',
         description: "Need a place to rent? Our rental experts will assist you in finding the ideal rental property, whether it's a cozy apartment or a spacious house.",
-        backgroundImage: './images/choose-card-img1.jpg',
-        logo: './images/icons/house-door.svg'
+        backgroundImage: './assets/images/choose-card-img1.jpg',
+        logo: './assets/icons/house-door.svg'
     },
     {
         id: 2,
         title: 'Buying',
         description: 'Looking to buy your dream home? Our team will guide you through the process, ensuring you find the perfect property that meets your needs and budget.',
-        backgroundImage: './images/choose-card-img2.jpg',
-        logo: './images/icons/cart2.svg'
+        backgroundImage: './assets/images/choose-card-img2.jpg',
+        logo: './assets/icons/cart2.svg'
     },
     {
         id: 3,
         title: 'Selling',
         description: "Ready to sell your property? We'll help you market your home effectively, attract qualified buyers, and navigate the selling process with ease.",
-        backgroundImage: './images/choose-card-img3.jpg',
-        logo: './images/icons/cash.svg'
+        backgroundImage: './assets/images/choose-card-img3.jpg',
+        logo: './assets/icons/cash.svg'
     }
 ];
 
