11e4fd911886a5122804b16802f5bbeec9ac2345
Ionut Cora <cora.ionut92@yahoo.com>
2024-02-05 11:23:54 +0000

    react routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Sale from './pages/Sale';
+import SaleProperty from './pages/SaleProperty';
+import Rent from './pages/Rent';
+import RentProperty from './pages/RentProperty';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Montserrat',
+      'sans-serif'
+    ].join(','),
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sale' element={<Sale />} />
+        <Route path='/saleproperty/:id' element={<SaleProperty />} />
+        <Route path='/rent' element={<Rent />} />
+        <Route path='/rentproperty/:id' element={<RentProperty />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
