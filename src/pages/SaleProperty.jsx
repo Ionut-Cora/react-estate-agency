e5825b257e16a8690ff6ff83969dc81f1aa8f1e0
Ionut Cora <cora.ionut92@yahoo.com>
2024-02-05 11:16:46 +0000

    sale property page

---
+++ src/pages/SaleProperty.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import saleItems from '../assets/saleItems';
+import Navigation from '../components/Navigation';
+import { useParams } from 'react-router-dom';
+import Footer from '../components/Footer';
+import RentSaleProperty from '../components/RentSaleComponents/RentSaleProperty';
+import MortgageCalculator from '../components/MortgageCalculator';
+import { Box } from '@mui/material';
+
+function SaleProperty() {
+
+  const {id} = useParams();
+  const [saleProperty, setSaleProperty] = useState(null);
+
+  useEffect(() => {
+
+    let saleProperty = saleItems.find(saleProperty => saleProperty.id === parseInt(id));
+
+    if (saleProperty) {
+      setSaleProperty(saleProperty);
+    }
+
+  }, [id])
+
+  return (
+    <Box>
+      <Navigation navPosition='relative' textColor='black' />
+
+      {
+        saleProperty ? (
+          <Box>
+            <RentSaleProperty propertyAddress={saleProperty.address} propertyImage={saleProperty.image1} propertyImages={saleProperty.images} propertyPrice={saleProperty.price} propertyLongDescription={saleProperty.longDescription} propertyBedrooms={saleProperty.bedrooms} propertyBathrooms={saleProperty.bathrooms} propertyLivingrooms={saleProperty.livingrooms} />
+  
+            <MortgageCalculator fullPropertyPrice={saleProperty.price} />
+          </Box>
+        ) : (
+          null
+        )
+      }
+      
+      <Footer />
+    </Box>
+  )
+}
+
+export default SaleProperty;
