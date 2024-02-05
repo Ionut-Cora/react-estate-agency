ff397db48d8453f77b597428e3e95b885c009b8f
Ionut Cora <cora.ionut92@yahoo.com>
2024-02-05 11:15:26 +0000

    sale page

---
+++ src/pages/Sale.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import SaleItem from '../components/SaleItem';
+import Navigation from '../components/Navigation';
+import { Container } from '@mui/material';
+import SecondHeader from '../components/SecondHeader';
+import SaleHeaderImg from '../assets/images/sale-header-img.jpg';
+import saleItems from '../assets/saleItems';
+import Footer from '../components/Footer';
+
+function Sale() {
+  return (
+    <div>
+        <Navigation navPosition='absolute' textColor='inherit' />
+        <SecondHeader title='Properties for sale' img={SaleHeaderImg} />
+        <Container>
+          {
+                saleItems.map(item => <SaleItem key={item.id} id={item.id} title={item.title} address={item.address} price={item.price} shortDescription={item.shortDescription} bedrooms={item.bedrooms} bathrooms={item.bathrooms} livingrooms={item.livingrooms} img={item.image1} />)
+            }
+        </Container>
+        <Footer />
+    </div>
+  )
+}
+
+export default Sale;
