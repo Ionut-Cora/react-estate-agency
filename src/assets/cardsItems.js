7bad8e69bcd1517d6de53500f1b611bf3595751b
Ionut Cora <cora.ionut92@yahoo.com>
2024-02-07 14:42:29 +0000

    recently added cards links fix

--- src/assets/cardsItems.js
+++ src/assets/cardsItems.js
@@ -4,21 +4,21 @@ let cardsItems = [
         address: "High street, London",
         price: "270,000",
         img: "./assets/images/img1.jpg",
-        url: "/saleproperty/1"
+        url: "/#/saleproperty/1"
     },
     {
         title: "1 bedroom flat",
         address: "Main street, London",
         price: "340,000",
         img: "./assets/images/img2.jpg",
-        url: "/saleproperty/2"
+        url: "/#/saleproperty/2"
     },
     {
         title: "2 bedroom flat",
         address: "Oxford street, London",
         price: "320,000",
         img: "./assets/images/img3.jpg",
-        url: "/saleproperty/4"
+        url: "/#/saleproperty/4"
     },
 ];
 
