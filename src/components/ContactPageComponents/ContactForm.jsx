155767a58231b6cadba8ec9dcb58a357d58ade8a
Ionut Cora <cora.ionut92@yahoo.com>
2025-01-16 18:15:42 +0000

    contact form changes

--- src/components/ContactPageComponents/ContactForm.jsx
+++ src/components/ContactPageComponents/ContactForm.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
+import './Contact.css';
 import { Box, Button } from '@mui/material';
 import TextField from '@mui/material/TextField';
 
 function ContactForm() {
   return (
-    <Box sx={{
-        width: { sx: '100%', md: '60%' }
-    }}>
-        <form style={{
-            display: 'flex',
-            flexDirection: 'column'
+    <div id="contact-form">
+        <Box sx={{
+            width: { xs: '100%', sm: '60%' }
         }}>
-            <TextField id="first-name" label="First Name" variant="outlined" sx={{marginBottom: '1rem'}} />
-            <TextField id="last-name" label="Last Name" variant="outlined" sx={{marginBottom: '1rem'}} />
-            <TextField id="address" label="Your Address" variant="outlined" sx={{marginBottom: '1rem'}} />
-            <TextField id="comment" label="Your Comment" multiline variant="outlined" maxRows={4} sx={{marginBottom: '1rem'}} />
-            <Box>
-                <Button variant='contained' 
+            <form style={{
+                display: 'flex',
+                flexDirection: 'column'
+            }}>
+                <TextField id="first-name" label="First Name" variant="outlined" sx={{marginBottom: '1rem'}} />
+                <TextField id="last-name" label="Last Name" variant="outlined" sx={{marginBottom: '1rem'}} />
+                <TextField id="address" label="Your Address" variant="outlined" sx={{marginBottom: '1rem'}} />
+                <TextField id="comment" label="Your Comment" multiline variant="outlined" maxRows={4} sx={{marginBottom: '1rem'}} />
+                <Box>
+                    <Button 
+                        variant='contained' 
                         color='warning' 
                         size='large'
-                >
-                    Submit
-                </Button>
-            </Box>
-        </form>
-    </Box>
+                        sx={{
+                            textTransform: 'capitalize'
+                        }}
+                    >
+                        Submit
+                    </Button>
+                </Box>
+            </form>
+        </Box>
+    </div>
   )
 }
 
